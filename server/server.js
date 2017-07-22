2f41b99d3c8c042c65b48fbe470688e366104dde
mlnck <jmelnick@jmelnick.com>
2017-07-22 14:55:55 -0400

    More Server build out - client pages stubbed

--- server/server.js
+++ server/server.js
@@ -4,31 +4,31 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import path from 'path';
 import IntlWrapper from '../config/intl/client/IntlWrapper';
-//
+
 // Webpack Requirements
 import webpack from 'webpack';
 import config from '../config/webpack/webpack.dev';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-//
-// // Initialize the Express App
-// const app = new Express();
-//
-// // Run Webpack dev server in development mode
-// if (process.env.NODE_ENV === 'development') {
-//   const compiler = webpack(config);
-//   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-//   app.use(webpackHotMiddleware(compiler));
-// }
-//
-// // React And Redux Setup
-// import { configureStore } from '../client/store';
-// import { Provider } from 'react-redux';
-// import React from 'react';
-// import { renderToString } from 'react-dom/server';
-// import { match, RouterContext } from 'react-router';
-// import Helmet from 'react-helmet';
-//
+
+// Initialize the Express App
+const app = new Express();
+
+// Run Webpack dev server in development mode
+if (process.env.NODE_ENV === 'development') {
+  const compiler = webpack(config);
+  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+  app.use(webpackHotMiddleware(compiler));
+}
+
+// React And Redux Setup
+import { configureStore } from '../client/store'; //--> from react-boilerplate
+import { Provider } from 'react-redux';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import Helmet from 'react-helmet';
+
 // // Import required modules
 // import routes from '../client/routes';
 // import { fetchComponentData } from './util/fetchData';
