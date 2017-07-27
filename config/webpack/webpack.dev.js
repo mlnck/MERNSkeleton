4c67ffa4c0394d7e37d8999c8b9c645c48f8226a
mlnck <jmelnick@jmelnick.com>
2017-07-27 05:08:05 -0400

    minor

--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -3,7 +3,6 @@ var webpack = require('webpack');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
-  // entry: './client/index.js',
   entry: {
     app: [
       'eventsource-polyfill',
