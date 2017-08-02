ec7a16895feb7b12b7f26d57b5e11584099d78c4
mlnck <jmelnick@jmelnick.com>
2017-08-02 02:28:51 -0400

    Removing Unused Elements

--- client/reducers.js
+++ client/reducers.js
@@ -7,8 +7,8 @@ import { fromJS } from 'immutable';
 import { combineReducers } from 'redux-immutable';
 import { LOCATION_CHANGE } from 'react-router-redux';
 
-import globalReducer from './containers/App/reducer';
-import languageProviderReducer from './containers/LanguageProvider/reducer';
+import globalReducer from './containers/Root/reducer';
+// import languageProviderReducer from './containers/LanguageProvider/reducer';
 
 /*
  * routeReducer
@@ -45,7 +45,7 @@ export default function createReducer(asyncReducers) {
   return combineReducers({
     route: routeReducer,
     global: globalReducer,
-    language: languageProviderReducer,
+    // language: languageProviderReducer,
     ...asyncReducers,
   });
 }
