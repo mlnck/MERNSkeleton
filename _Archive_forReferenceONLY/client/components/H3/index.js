b4cf356362e3221ef5691347db6b5c6f59f769ab
mlnck <jmelnick@jmelnick.com>
2017-09-21 07:47:58 -0400

    Removing Archive Folder

--- _Archive_forReferenceONLY/client/components/H3/index.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const H3 = styled.h3`
-  font-size: 2em;
-  margin-bottom: 0.25em;
-`;
-
-export default H3;
--- _Archive_forReferenceONLY/server/routes/skeleton.routes.js
+++
@@ -1,9 +0,0 @@
-const SkeletonController = require('../controllers/skeleton.controller');
-const express = require('express');
-const router = new express.Router();
-
-// Get all Skeletons
-router.get('/', SkeletonController.getSkeletons);
-
-module.exports = router;
-// export default router;
