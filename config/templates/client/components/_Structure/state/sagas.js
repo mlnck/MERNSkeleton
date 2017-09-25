30d2aa1c318705dc56e2631f53a60089b19af880
mlnck <jmelnick@jmelnick.com>
2017-09-25 17:57:02 -0400

    updated saga template

--- config/templates/client/components/_Structure/state/sagas.js
+++ config/templates/client/components/_Structure/state/sagas.js
@@ -1,4 +1,14 @@
-/*optional file - ask on component/container create if needed*/
 // https://redux-saga.js.org/docs/api/
 import { delay } from 'redux-saga';
 import { put, call, takeLatest, all } from 'redux-saga/effects';
+
+// import {  } from './constants';
+
+
+// single entry point to start all Sagas at once
+export default function* xxxSaga()
+{
+  yield all([
+    //arrayOfSagaFunctions()
+  ]);
+}
